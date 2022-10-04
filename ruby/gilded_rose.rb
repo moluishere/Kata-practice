53c6a54cea5825e502d61e98370803da230b6edc
Yvonne <moluishere@gmail.com>
2022-10-05 01:11:30 +0800

    Kata practice 2nd & 3rd
    1. 由小範圍開始修改(預算子整理、if 語句整理)
    2. 修改成正面表達
    3. when...case... 把判斷的標的找出來(這裡是 item.name)
    4. 先使用 else 把所有判斷結果丟進去，再一個一個拉出來整理
    5. 注意要邊整理語法邊跑測試，確保修改正確

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -5,77 +5,29 @@ class GildedRose
   end
 
 
-  def update_quality()
+def update_quality()
     @items.each do |item|
       case item.name
       when 'Aged Brie'
-        item.sell_in -= 1
         item.quality += 1 if item.quality < 50
-        item.quality += 1 if item.sell_in < 0 && item.quality < 50
-      # item.quality = item.quality > 50 ? 50 : item.quality
+        item.sell_in -= 1
+        item.quality += 1 if item.quality < 50 && item.sell_in < 0
       when 'Backstage passes to a TAFKAL80ETC concert'
         item.quality += 1 if item.quality < 50
-        if item.sell_in < 11
-          item.quality += 1 if item.quality < 50
-        end
-        if item.sell_in < 6
-          item.quality += 1 if item.quality < 50
-        end
+        item.quality += 1 if item.quality < 50 && item.sell_in < 11
+        item.quality += 1 if item.quality < 50 && item.sell_in < 6
         item.sell_in -= 1
-        item.quality -= item.quality if item.sell_in < 0
+        item.quality = 0 if item.sell_in < 0
       when 'Sulfuras, Hand of Ragnaros'
+      else
+        item.quality -= 1 if item.quality > 0
+        item.sell_in -= 1
+        item.quality -= 1 if item.quality > 0 && item.sell_in < 0
       end
     end
   end
 end
 
-
-#       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-#         if item.quality > 0
-#           if item.name != "Sulfuras, Hand of Ragnaros"
-#             item.quality -=  1
-#           end
-#         end
-#       else
-#         if item.quality < 50
-#           item.quality += 1
-#           if item.name == "Backstage passes to a TAFKAL80ETC concert"
-#             if item.sell_in < 11
-#               if item.quality < 50
-#                 item.quality += 1
-#               end
-#             end
-#             if item.sell_in < 6
-#               if item.quality < 50
-#                 item.quality += 1
-#               end
-#             end
-#           end
-#         end
-#       end
-#       if item.name != "Sulfuras, Hand of Ragnaros"
-#         item.sell_in -= 1
-#       end
-#       if item.sell_in < 0
-#         if item.name != "Aged Brie"
-#           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-#             if item.quality > 0
-#               if item.name != "Sulfuras, Hand of Ragnaros"
-#                 item.quality -= 1
-#               end
-#             end
-#           else
-#             item.quality -= item.quality
-#           end
-#         else
-#           if item.quality < 50
-#             item.quality += 1
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
 
 class Item
   attr_accessor :name, :sell_in, :quality
