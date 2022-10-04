fd853f48edfcae7efefd0eff71310d7901aac1cf
Yvonne <moluishere@gmail.com>
2022-10-05 01:01:35 +0800

    Kata practice 1st
    9/15 with Jennifer & Mingo

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,54 +4,78 @@ class GildedRose
     @items = items
   end
 
+
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+      case item.name
+      when 'Aged Brie'
+        item.sell_in -= 1
+        item.quality += 1 if item.quality < 50
+        item.quality += 1 if item.sell_in < 0 && item.quality < 50
+      # item.quality = item.quality > 50 ? 50 : item.quality
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        item.quality += 1 if item.quality < 50
+        if item.sell_in < 11
+          item.quality += 1 if item.quality < 50
         end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
+        if item.sell_in < 6
+          item.quality += 1 if item.quality < 50
         end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        item.sell_in -= 1
+        item.quality -= item.quality if item.sell_in < 0
+      when 'Sulfuras, Hand of Ragnaros'
       end
     end
   end
 end
+
+
+#       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+#         if item.quality > 0
+#           if item.name != "Sulfuras, Hand of Ragnaros"
+#             item.quality -=  1
+#           end
+#         end
+#       else
+#         if item.quality < 50
+#           item.quality += 1
+#           if item.name == "Backstage passes to a TAFKAL80ETC concert"
+#             if item.sell_in < 11
+#               if item.quality < 50
+#                 item.quality += 1
+#               end
+#             end
+#             if item.sell_in < 6
+#               if item.quality < 50
+#                 item.quality += 1
+#               end
+#             end
+#           end
+#         end
+#       end
+#       if item.name != "Sulfuras, Hand of Ragnaros"
+#         item.sell_in -= 1
+#       end
+#       if item.sell_in < 0
+#         if item.name != "Aged Brie"
+#           if item.name != "Backstage passes to a TAFKAL80ETC concert"
+#             if item.quality > 0
+#               if item.name != "Sulfuras, Hand of Ragnaros"
+#                 item.quality -= 1
+#               end
+#             end
+#           else
+#             item.quality -= item.quality
+#           end
+#         else
+#           if item.quality < 50
+#             item.quality += 1
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
 
 class Item
   attr_accessor :name, :sell_in, :quality
